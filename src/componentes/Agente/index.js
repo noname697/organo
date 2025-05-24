f07ab6f3655c171e1fa73b187b6c4ce69ed4a345
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-05-24 20:17:23 -0300

    criado card de agente

---
+++ src/componentes/Agente/index.js
@@ -0,0 +1,17 @@
+import "./Agente.css"
+
+const Agente = () => {
+    return (
+        <div className="agente">
+            <div className="cabecalho">
+                <img src="https://github.com/noname697.png" alt="Arthur Manenti"/>
+            </div>
+            <div className="rodape">
+                <h4>Arthur</h4>
+                <h5>Duelista</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Agente
--- src/componentes/Funcao/index.js
+++ src/componentes/Funcao/index.js
@@ -1,11 +1,17 @@
-import './Funcao.css'
+import Agente from "../Agente";
+import "./Funcao.css";
 
 const Funcao = (props) => {
-    return(
-        <section className='funcao' style={{ backgroundColor: props.corSecundaria }}>
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-        </section>
-    )
+  return (
+    <section
+      className="funcao"
+      style={{ backgroundColor: props.corSecundaria }}
+    >
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <Agente />
+      <Agente />
+    </section>
+  );
 };
 
 export default Funcao;
