509553a3b8a7c648b0f56198fef5fa7d0d731a82
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-05-24 13:39:00 -0300

    começando a pegar os valores dos inputs

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,22 @@
-import "./CampoTexto.css"
+import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
+  let valor = "arthur";
+
+  const aoDigitado = (e) => {
+    valor = e.target.value;
+    console.log(valor);
+  };
+
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input required={props.obrigatorio} placeholder={props.placeholder}/>
+      <input
+        value={valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={`${props.placeholder}...`}
+      />
     </div>
   );
 };
--- src/componentes/formulario/index.js
+++ src/componentes/formulario/index.js
@@ -4,34 +4,34 @@ import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
-  const times = [
-    "Pedagógico",
-    "SISAE",
-    "TI",
-    "Extensão",
-    "Secretaria",
-    "AEE",
-    "Infraestrutura",
-    "Direções",
-    "Outros",
-  ];
+  const funcoes = ["Duelista", "Iniciador", "Controlador", "Suporte"];
 
   const aoSalvar = (e) => {
     e.preventDefault();
-    console.log("Form foi submetido")
-  }
+    console.log("Form foi submetido");
+  };
 
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
-        <h2>Preencha os dados para criar o card do Profissional.</h2>
-        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto obrigatorio={false} label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
-        <Botao>
-          Criar Card
-        </Botao>
+        <h2>Preencha os dados para criar o card do Agente.</h2>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Genêro"
+          placeholder="Digite seu genêro"
+        />
+        <CampoTexto
+          obrigatorio={false}
+          label="Imagem"
+          placeholder="Digite o endereço da imagem"
+        />
+        <ListaSuspensa obrigatorio={true} label="Função" itens={funcoes} />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
