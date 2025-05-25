050342fd2bb8bf76c1db4e072aa2703c0ff710c9
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-05-24 21:52:42 -0300

    Renderização Condicional

--- src/componentes/Funcao/index.js
+++ src/componentes/Funcao/index.js
@@ -3,15 +3,24 @@ import "./Funcao.css";
 
 const Funcao = (props) => {
   return (
-    <section
-      className="funcao"
-      style={{ backgroundColor: props.corSecundaria }}
-    >
-      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-      <div className="agentes">
-      {props.agentes.map(agente => <Agente key={agente.nome} nome={agente.nome} funcao={agente.funcao} imagem={agente.imagem}/>)}
-      </div>
-    </section>
+    props.agentes.length > 0 && (
+      <section
+        className="funcao"
+        style={{ backgroundColor: props.corSecundaria }}
+      >
+        <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+        <div className="agentes">
+          {props.agentes.map((agente) => (
+            <Agente
+              key={agente.nome}
+              nome={agente.nome}
+              funcao={agente.funcao}
+              imagem={agente.imagem}
+            />
+          ))}
+        </div>
+      </section>
+    )
   );
 };
 
