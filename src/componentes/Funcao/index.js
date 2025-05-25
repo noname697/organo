00b79427aad3102fb7056cdd2bacf36a025b7d0e
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-05-24 21:57:14 -0300

    Renderização condicional com operador ternário

--- src/componentes/Funcao/index.js
+++ src/componentes/Funcao/index.js
@@ -2,16 +2,25 @@ import Agente from "../Agente";
 import "./Funcao.css";
 
 const Funcao = (props) => {
-  return (
+  return props.agentes.length > 0 ? (
     <section
       className="funcao"
       style={{ backgroundColor: props.corSecundaria }}
     >
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
       <div className="agentes">
-      {props.agentes.map(agente => <Agente key={agente.nome} nome={agente.nome} funcao={agente.funcao} imagem={agente.imagem}/>)}
+        {props.agentes.map((agente) => (
+          <Agente
+            key={agente.nome}
+            nome={agente.nome}
+            funcao={agente.funcao}
+            imagem={agente.imagem}
+          />
+        ))}
       </div>
     </section>
+  ) : (
+    ""
   );
 };
 
