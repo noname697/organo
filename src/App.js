25e29cd771585f093793f7404a75bfff85a232bb
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-05-25 11:59:47 -0300

    Correção bug vercel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Banner from "./componentes/Banner";
-import Formulario from "./componentes/Formulario";
+import Formulario from "./componentes/Formulario/index.js";
 import Funcao from "./componentes/Funcao";
 import Footer from "./componentes/Footer";
 
