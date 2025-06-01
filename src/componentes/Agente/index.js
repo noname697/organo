6bf45d3a70bf07c7c0165109f907e9beb03baaab
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-01 14:08:26 -0300

    tamanho do ícone deletar

--- src/componentes/Agente/index.js
+++ src/componentes/Agente/index.js
@@ -4,7 +4,7 @@ import { AiFillCloseCircle } from "react-icons/ai";
 const Agente = ({nome, imagem, funcao, corDeFundo, aoDeletar}) => {
     return (
         <div className="agente">
-            <AiFillCloseCircle className="deletar" onClick={aoDeletar}/>
+            <AiFillCloseCircle size={25} className="deletar" onClick={aoDeletar}/>
             <div className="cabecalho" style={{backgroundColor: corDeFundo}}>
                 <img src={imagem} alt="Arthur Manenti"/>
             </div>
