645172c229684f339e3ef9d3eb44e50085627161
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-01 13:47:20 -0300

    entendendo uma clusure

--- src/componentes/Funcao/index.js
+++ src/componentes/Funcao/index.js
@@ -1,23 +1,26 @@
 import Agente from "../Agente";
 import "./Funcao.css";
 
-const Funcao = (props) => {
-  return props.agentes.length > 0 ? (
+const Funcao = ({nome, corPrimaria, corSecundaria, agentes, aoDeletar}) => {
+  return agentes.length > 0 ? (
     <section
       className="funcao"
-      style={{ backgroundColor: props.corSecundaria }}
+      style={{ backgroundColor: corSecundaria }}
     >
-      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
       <div className="agentes">
-        {props.agentes.map((agente) => (
-          <Agente
-          corDeFundo={props.corPrimaria}
-            key={agente.nome}
-            nome={agente.nome}
-            funcao={agente.funcao}
-            imagem={agente.imagem}
-          />
-        ))}
+        {agentes.map((agente) => {
+          return (
+            <Agente
+              corDeFundo={corPrimaria}
+              key={agente.nome}
+              nome={agente.nome}
+              funcao={agente.funcao}
+              imagem={agente.imagem}
+              aoDeletar={aoDeletar}
+            />
+          );
+        })}
       </div>
     </section>
   ) : (
