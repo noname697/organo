32cd8374181900e736a0fa8974f4425924fa63dd
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-05 13:52:30 -0300

    useEffect com fetch para api json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 import Funcao from "./componentes/Funcao";
@@ -29,171 +29,15 @@ function App() {
       cor: "#B6F25C",
     },
   ]);
-
-  const agentesIniciais = [
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Lucas",
-      genero: "Masculino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Duelista",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Marina",
-      genero: "Feminino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Iniciador",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Rafaela",
-      genero: "Feminino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Controlador",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "João",
-      genero: "Masculino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Sentinela",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Isabela",
-      genero: "Feminino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Duelista",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Fernando",
-      genero: "Masculino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Iniciador",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Bianca",
-      genero: "Feminino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Controlador",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Carlos",
-      genero: "Masculino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Duelista",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Juliana",
-      genero: "Feminino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Sentinela",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "André",
-      genero: "Masculino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Controlador",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Camila",
-      genero: "Feminino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Iniciador",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Pedro",
-      genero: "Masculino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Duelista",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Fernanda",
-      genero: "Feminino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Sentinela",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Thiago",
-      genero: "Masculino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Iniciador",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Patrícia",
-      genero: "Feminino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Controlador",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Eduardo",
-      genero: "Masculino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Sentinela",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Larissa",
-      genero: "Feminino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Duelista",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Bruno",
-      genero: "Masculino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Controlador",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Vanessa",
-      genero: "Feminino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Iniciador",
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "Ricardo",
-      genero: "Masculino",
-      imagem: "https://github.com/noname697.png",
-      funcao: "Duelista",
-    },
-  ];
-
-  const [agentes, setAgentes] = useState(agentesIniciais);
+  
+  const [agentes, setAgentes] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8080/agentes')
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setAgentes(dados);
+      })
+  }, []) 
 
   const aoNovoAgenteAdicionado = (agente) => {
     setAgentes([...agentes, agente]);
